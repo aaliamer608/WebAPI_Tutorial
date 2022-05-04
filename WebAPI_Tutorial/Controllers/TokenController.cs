20de17144e37ef03d383f2ff2c98812f20370298
aaliamer608 <aaliamer608@gmail.com>
2022-05-04 16:20:55 -0400

    Copied Authorization but not implemented

---
+++ WebAPI_Tutorial/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+namespace WebAPI_Tutorial.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [Authorize]
+        public IHttpActionResult Authorize()
+        {
+            return Ok("Authorized");
+        }
+    }
+}
