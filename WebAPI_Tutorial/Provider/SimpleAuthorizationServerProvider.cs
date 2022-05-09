d89563edfec29d21ae9b621dc432822f33e9cfae
aaliamer608 <aaliamer608@gmail.com>
2022-05-08 22:55:16 -0400

    StartUp For OAuth

--- WebAPI_Tutorial/Provider/SimpleAuthorizationServerProvider.cs
+++ WebAPI_Tutorial/Provider/SimpleAuthorizationServerProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin.Security.OAuth;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using Microsoft.Owin.Security;
 
 namespace WebAPI_Tutorial
 {
@@ -15,6 +16,14 @@ namespace WebAPI_Tutorial
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             context.Validated(new ClaimsIdentity(context.Options.AuthenticationType));
+        }
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            var requestIdentity = new ClaimsIdentity(context.Ticket.Identity);
+            requestIdentity.AddClaim(new Claim("newClaim", "newValue"));
+            var newTicket = new AuthenticationTicket(requestIdentity, context.Ticket.Properties);
+            context.Validated(newTicket);
+            return Task.FromResult<object>(null);
         }
     }
 }
