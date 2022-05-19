72edac80174acb7af92f7954b37b1b9e8696b64d
aaliamer608 <aaliamer608@gmail.com>
2022-05-19 02:12:54 -0400

    Updated DB Context For Users PK

--- DomainEntities/Models/WebApiEntities.Designer.cs
+++ DomainEntities/Models/WebApiEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Bus-Boi\Desktop\ITLize Orientation\WebAPI\Tutorial_1\WebAPI_Tutorial\DomainEntities\Models\DBEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Bus-Boi\Desktop\ITLize Orientation\WebAPI\Tutorial_1\WebAPI_Tutorial\DomainEntities\Models\WebApiEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
