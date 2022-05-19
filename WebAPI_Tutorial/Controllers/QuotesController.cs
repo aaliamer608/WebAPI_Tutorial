c781127bdc4de968054c6ca7da2b3418b2550a18
aaliamer608 <aaliamer608@gmail.com>
2022-05-18 23:40:21 -0400

    Managed Some User Service

--- WebAPI_Tutorial/Controllers/QuotesController.cs
+++ WebAPI_Tutorial/Controllers/QuotesController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebAPI_Tutorial;
 
 
 namespace WebAPI_Tutorial.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class QuotesController : ApiController
     {
 
--- WebAPI_Tutorial/Controllers/UsersController.cs
+++ WebAPI_Tutorial/Controllers/UsersController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebAPI_Tutorial;
 
 
 namespace WebAPI_Tutorial.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
 
@@ -46,7 +48,28 @@ namespace WebAPI_Tutorial.Controllers
         }
 
 
-        //POST
+        //POST Quote
+        [HttpPost]
+        public void PostUser([FromBody] UserDTO userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    UserDTO userDTO = mapper.Map<UserDTO>(userModel);
+                    userService.PostUser(userDTO);
+
+                }
+                catch (Exception)
+                {
+                    BadRequest();
+                }
+            }
+        }
+
+
+
+
         //PATCH
         //DELETE
         //PUT
